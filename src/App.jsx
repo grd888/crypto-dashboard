0a3bff5e1a97763ab66e88308b9acdac5bb8f5dc
Greg Delgado <grd888@gmail.com>
2025-08-08 07:53:25 +0800

    fetch initial coins data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
 const App = () => {
-  return ( <div>
-    <h1>🚀 Crypto Dash</h1>
-  </div> );
-}
- 
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then(res => {
+      if (!res.ok) throw new Error("Failed to fetch data");
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      setCoins(data);
+    })
+    .catch(err => {
+      setError(err.message);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, []);
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+    </div>
+  );
+};
+
 export default App;
