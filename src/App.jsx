936862e2e40feb4da321230f9d99a245d61cfc10
Greg Delgado <grd888@gmail.com>
2025-08-09 06:13:24 +0800

    Display the coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
 const App = () => {
   const [coins, setCoins] = useState([]);
@@ -20,27 +21,47 @@ const App = () => {
       } finally {
         setLoading(false);
       }
-    }
+    };
     fetchCoins();
-    //   fetch(API_URL)
-    // .then(res => {
-    //   if (!res.ok) throw new Error("Failed to fetch data");
-    //   return res.json();
-    // })
-    // .then(data => {
-    //   console.log(data);
-    //   setCoins(data);
-    // })
-    // .catch(err => {
-    //   setError(err.message);
-    // })
-    // .finally(() => {
-    //   setLoading(false);
-    // })
   }, []);
+
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error}</div>}
+
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <div
+              key={coin.id}
+              className="coin-card"
+            >
+              <div className="coin-header">
+                <img
+                  className="coin-image"
+                  src={coin.image}
+                  alt={coin.name}
+                />
+                <div className="coin-info">
+                  <h2>{coin.name}</h2>
+                  <p className="symbol">{coin.symbol.toUpperCase()}</p>
+                </div>
+              </div>
+              <p>Price: ${coin.current_price.toLocaleString()}</p>
+              <p
+                className={
+                  coin.price_change_percentage_24h > 0 ? "positive" : "negative"
+                }
+              >
+                {coin.price_change_percentage_24h.toFixed(2)} %
+              </p>
+              <p>Market Cap: ${coin.market_cap.toLocaleString()}</p>
+            </div>
+          ))}
+        </main>
+      )}
     </div>
   );
 };
