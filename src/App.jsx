c3ce01df8708804d6933a4296ebc6e79bd9ad140
Greg Delgado <grd888@gmail.com>
2025-08-09 11:08:18 +0800

    create 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import AboutPage from "./pages/about";
 import Header from "./components/Header";
+import NotFoundPage from "./pages/not-found";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -56,6 +57,10 @@ const App = () => {
         <Route
           path="/about"
           element={<AboutPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
         />
       </Routes>
     </>
