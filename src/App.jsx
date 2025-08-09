04c36238e87f78ff48492f9db1611a6378671556
Greg Delgado <grd888@gmail.com>
2025-08-09 10:51:05 +0800

    add an about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
+import Header from "./components/Header";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -32,24 +34,31 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            filter={filter}
-            setFilter={setFilter}
-            limit={limit}
-            setLimit={setLimit}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            loading={loading}
-            error={error}
-          />
-        }
-      />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route
+          path="/about"
+          element={<AboutPage />}
+        />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1>About Crypto Dash</h1>
+      <p>Created by Greg Delgado</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+} 
+ 
+export default AboutPage;
